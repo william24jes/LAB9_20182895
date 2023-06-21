c4d77c69403467f7f295979bc62a475b32ac09be
william <william24jes@hotmail.com>
2023-06-20 19:12:15 -0500

    1er commit

---
+++ LAB9_20182895/src/main/java/com/example/lab9_20182895/Lab920182895Application.java
@@ -0,0 +1,13 @@
+package com.example.lab9_20182895;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab920182895Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab920182895Application.class, args);
+    }
+
+}
---
+++ LAB9_20182895/src/test/java/com/example/lab9_20182895/Lab920182895ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab9_20182895;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab920182895ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
